ec3994d80c480740a1f81024697d57ea17a1ed68
Joanna <j.walczak94@interia.pl>
2023-03-23 15:02:11 +0100

    first version

---
+++ mobileMenu.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const menuMobile = document.getElementById("menu"),
+  overlay = document.querySelector(".overlay"),
+  listItems = document.querySelectorAll(".list-items"),
+  btnArrows = document.querySelectorAll(".btn-arrow"),
+  btns = document.querySelectorAll(".btn-links");
+
+function mobileMenu(menu) {
+  menu.classList.toggle("change");
+  menuMobile.classList.toggle("active");
+
+  if (menuMobile.classList.contains("active")) {
+    overlay.style.display = "block";
+  } else {
+    overlay.style.display = "none";
+    featuresMenu.classList.remove("active-list");
+    companyMenu.classList.remove("active-list");
+  }
+
+  btns.forEach(function (btn, key) {
+    btn.addEventListener("click", function () {
+      listItems[key].classList.toggle("active-list");
+      btnArrows[key].src = "./images/icon-arrow-up.svg";
+
+      if (listItems[key].classList.contains("active-list")) {
+        btnArrows[key].src = "./images/icon-arrow-down.svg";
+      }
+    });
+  });
+}
